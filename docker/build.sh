34816d035c06080094c90a7a626e02f31cbedd02
Fukuda-glb <fukuda.takashi@iofit.co.jp>
2022-11-10 16:26:05 +0900

    fix: docker/dockerfile modified; build.sh changed to my hub build/push;

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT .
-docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT
+docker build -t fukudatakashi/jetlinks-ui-pro:2.0.0-SNAPSHOT .
+docker push fukudatakashi/jetlinks-ui-pro:2.0.0-SNAPSHOT
+#docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT .
+#docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT
