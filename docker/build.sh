39f7026f6045774eaf988ec44cee79445d596ccd
Fukuda-glb <fukuda.takashi@iofit.co.jp>
2022-11-24 12:47:12 +0900

    Revert "fix: docker/dockerfile modified; build.sh changed to my hub build/push;"
    This reverts commit 34816d035c06080094c90a7a626e02f31cbedd02.

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-docker build -t fukudatakashi/jetlinks-ui-pro:2.0.0-SNAPSHOT .
-docker push fukudatakashi/jetlinks-ui-pro:2.0.0-SNAPSHOT
-#docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT .
-#docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT
+docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT .
+docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT
